2d88d9a191aa79d6ed45803df68e1b8bdd453196
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-02-05 22:28:11 -0500

    signup component using state

--- frontend/components/auth/SignupComponent.js
+++ frontend/components/auth/SignupComponent.js
@@ -1,17 +1,40 @@
+import { useState } from 'react'
+
 const SignupComponent = () => {
+	const [values, setValues] = useState({
+		name: '',
+		email: '',
+		password: '',
+		error: '',
+		loading: false,
+		message: '',
+		showForm: true
+	})
+
+	const { name, email, password, error, loading, message, showForm } = values
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log('handle submit');
+		console.table({ name, email, password, error, loading, message, showForm })
 	};
-	const handleChange = (e) => {
-		console.log(e.target.value);
+	const handleChange = name => e => {
+		setValues({...values, error: false, [name]: e.target.value})
 	};
 
 	const signupForm = () => {
 		return (
 			<form onSubmit={handleSubmit}>
 				<div className="form-group">
-					<input onChange={handleChange} className="form-control" type="text" placeholder="Type your name" />
+					<input onChange={handleChange('name')} value={name}className="form-control" type="text" placeholder="Type your name" />
+				</div>
+				<div className="form-group">
+					<input onChange={handleChange('email')} value={email} className="form-control" type="email" placeholder="Type your email" />
+				</div>
+				<div className="form-group">
+					<input onChange={handleChange('password')} value={password} className="form-control" type="password" placeholder="Type your password" />
+				</div>
+				<div>
+					<button className="btn btn-primary">Signup</button>
 				</div>
 			</form>
 		);
