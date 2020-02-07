37476322a943331c0cc371b0bb5cf3f48278f1c8
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-02-06 23:52:32 -0500

    dashboard link and protected routes

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -51,6 +51,22 @@ const Header = () => {
 								</NavItem>
 							</React.Fragment>
 						)}
+						{isAuth() &&
+						isAuth().role === 0 && (
+							<NavItem>
+								<Link href="/user">
+									<NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+								</Link>
+							</NavItem>
+						)}
+						{isAuth() &&
+						isAuth().role === 1 && (
+							<NavItem>
+								<Link href="/admin">
+									<NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+								</Link>
+							</NavItem>
+						)}
 						{isAuth() && (
 							<NavItem>
 								<NavLink
