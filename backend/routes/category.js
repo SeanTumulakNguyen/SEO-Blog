f8ba2a3010bd40b7b4c073a5be1439672d7ca85e
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-02-07 23:19:07 -0500

    create delete read for Category

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { create } = require('../controllers/category');
+const { create, list, read, remove } = require('../controllers/category');
 
 // validator
 
 const { runValidation } = require('../validators');
 const { categoryCreateValidator } = require('../validators/category');
-const { requireSignin, adminMiddlware } = require('../controllers/auth')
+const { requireSignin, adminMiddlware } = require('../controllers/auth');
 
 router.post('/category', categoryCreateValidator, runValidation, requireSignin, adminMiddlware, create);
+router.get('/categories', list);
+router.get('/category/:slug', read);
+router.delete('/category/:slug', requireSignin, adminMiddlware, remove);
 
 module.exports = router;
