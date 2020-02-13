6d88ad192450a3c853df4b2c23ba5be92824f5bb
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-02-12 22:19:16 -0500

    tag crud

--- frontend/components/crud/Tag.js
+++ frontend/components/crud/Tag.js
@@ -2,9 +2,9 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 import { isAuth, getCookie } from '../../actions/auth';
-import { create, getCategories, removeCategory } from '../../actions/category';
+import { create, getTags, removeTag } from '../../actions/tag';
 
-const Category = () => {
+const Tag = () => {
 	const [
 		values,
 		setValues
@@ -12,59 +12,58 @@ const Category = () => {
 		name: '',
 		error: false,
 		success: false,
-		categories: [],
+		tags: [],
 		removed: false,
 		reload: false
 	});
 
-	const { name, error, success, categories, removed, reload } = values;
+	const { name, error, success, tags, removed, reload } = values;
 	const token = getCookie('token');
 
 	useEffect(
 		() => {
-			loadCategories();
+			loadTags();
 		},
 		[
 			reload
 		]
 	);
 
-	const loadCategories = () => {
-		getCategories().then((data) => {
+	const loadTags = () => {
+		getTags().then((data) => {
 			if (data.error) {
 				console.log(data.error);
 			}
 			else {
-				setValues({ ...values, categories: data });
+				setValues({ ...values, tags: data });
 			}
 		});
 	};
 
-	const showCategories = () => {
-		return categories.map((c, i) => {
+	const showTags = () => {
+		return tags.map((t, i) => {
 			return (
 				<button
-					onDoubleClick={() => deleteConfirm(c.slug)}
+					onDoubleClick={() => deleteConfirm(t.slug)}
 					title="Double click to delete"
 					key={i}
 					className="btn btn-outline-primary mr-1 ml-1 mt-3"
 				>
-					{c.name}
+					{t.name}
 				</button>
 			);
 		});
 	};
 
 	const deleteConfirm = (slug) => {
-		let answer = window.confirm('Are you sure you want to delete this category?');
+		let answer = window.confirm('Are you sure you want to delete this tag?');
 		if (answer) {
-			deleteCategory(slug);
+			deleteTag(slug);
 		}
 	};
 
-	const deleteCategory = (slug) => {
-		// console.log('delete', slug)
-		removeCategory(slug, token).then((data) => {
+	const deleteTag = (slug) => {
+		removeTag(slug, token).then((data) => {
 			if (data.error) {
 				console.log(data.error);
 			}
@@ -76,7 +75,7 @@ const Category = () => {
 
 	const clickSubmit = (e) => {
 		e.preventDefault();
-		// console.log('create category', name);
+
 		create({ name }, token).then((data) => {
 			if (data.error) {
 				setValues({ ...values, error: data.error, success: false });
@@ -93,27 +92,27 @@ const Category = () => {
 
 	const showSuccess = () => {
 		if (success) {
-			return <p className="text-success">Category is created</p>;
+			return <p className="text-success">Tag is created</p>;
 		}
 	};
 
 	const showError = () => {
 		if (error) {
-			return <p className="text-danger">Category already exists</p>;
+			return <p className="text-danger">Tag already exists</p>;
 		}
 	};
 
 	const showRemoved = () => {
 		if (removed) {
-			return <p className="text-danger">Category is removed</p>;
+			return <p className="text-danger">Tag is removed</p>;
 		}
 	};
 
 	const mouseMoveHandler = (e) => {
-		setValues({...values, error:false, success:false, removed: ''})
-	}
+		setValues({ ...values, error: false, success: false, removed: '' });
+	};
 
-	const newCategoryForm = () => (
+	const newTagForm = () => (
 		<form onSubmit={clickSubmit}>
 			<div className="form-group">
 				<label className="text-muted">Name</label>
@@ -133,11 +132,11 @@ const Category = () => {
 			{showError()}
 			{showRemoved()}
 			<div onMouseMove={mouseMoveHandler}>
-				{newCategoryForm()}
-				{showCategories()}
+				{newTagForm()}
+				{showTags()}
 			</div>
 		</React.Fragment>
 	);
 };
 
-export default Category;
+export default Tag
--- frontend/pages/admin/crud/category-tag.js
+++ frontend/pages/admin/crud/category-tag.js
@@ -1,8 +1,8 @@
 import Layout from '../../../components/Layout';
 import Admin from '../../../components/auth/Admin';
-import Category from '../../../components/crud/Category'
+import Category from '../../../components/crud/Category';
+import Tag from '../../../components/crud/Tag';
 import Link from 'next/link';
-
 
 const CategoryTag = () => {
 	return (
@@ -13,11 +13,11 @@ const CategoryTag = () => {
 						<div className="col-md-12 pt-5 pb-5">
 							<h2>Manage Categories and Tags</h2>
 						</div>
-                        <div className="col-md-6">
+						<div className="col-md-6">
 							<Category />
 						</div>
 						<div className="col-md-6">
-							<p>Tag</p>
+							<Tag />
 						</div>
 					</div>
 				</div>
