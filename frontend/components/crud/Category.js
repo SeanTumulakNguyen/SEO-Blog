de764f53e3df2f35e44faf4b6cf628eca1ce37ee
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-02-12 20:13:40 -0500

    crud categories

--- frontend/components/crud/Category.js
+++ frontend/components/crud/Category.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 import { isAuth, getCookie } from '../../actions/auth';
-import { create } from '../../actions/category';
+import { create, getCategories, removeCategory } from '../../actions/category';
 
 const Category = () => {
 	const [
@@ -13,11 +13,66 @@ const Category = () => {
 		error: false,
 		success: false,
 		categories: [],
-		removed: false
+		removed: false,
+		reload: false
 	});
 
-	const { name, error, success, categories, removed } = values;
+	const { name, error, success, categories, removed, reload } = values;
 	const token = getCookie('token');
+
+	useEffect(
+		() => {
+			loadCategories();
+		},
+		[
+			reload
+		]
+	);
+
+	const loadCategories = () => {
+		getCategories().then((data) => {
+			if (data.error) {
+				console.log(data.error);
+			}
+			else {
+				setValues({ ...values, categories: data });
+			}
+		});
+	};
+
+	const showCategories = () => {
+		return categories.map((c, i) => {
+			return (
+				<button
+					onDoubleClick={() => deleteConfirm(c.slug)}
+					title="Double click to delete"
+					key={i}
+					className="btn btn-outline-primary mr-1 ml-1 mt-3"
+				>
+					{c.name}
+				</button>
+			);
+		});
+	};
+
+	const deleteConfirm = (slug) => {
+		let answer = window.confirm('Are you sure you want to delet this category?');
+		if (answer) {
+			deleteCategory(slug);
+		}
+	};
+
+	const deleteCategory = (slug) => {
+		// console.log('delete', slug)
+		removeCategory(slug, token).then((data) => {
+			if (data.error) {
+				console.log(data.error);
+			}
+			else {
+				setValues({ ...values, error: false, success: false, name: '', removed: !removed, reload: !reload });
+			}
+		});
+	};
 
 	const clickSubmit = (e) => {
 		e.preventDefault();
@@ -27,7 +82,7 @@ const Category = () => {
 				setValues({ ...values, error: data.error, success: false });
 			}
 			else {
-				setValues({ ...values, error: false, success: true, name: '' });
+				setValues({ ...values, error: false, success: true, name: '', removed: removed, reload: !reload });
 			}
 		});
 	};
@@ -35,6 +90,28 @@ const Category = () => {
 	const handleChange = (e) => {
 		setValues({ ...values, name: e.target.value, error: false, success: false, removed: '' });
 	};
+
+	const showSuccess = () => {
+		if (success) {
+			return <p className="text-success">Category is created</p>;
+		}
+	};
+
+	const showError = () => {
+		if (error) {
+			return <p className="text-danger">Category already exists</p>;
+		}
+	};
+
+	const showRemoved = () => {
+		if (removed) {
+			return <p className="text-danger">Category is removed</p>;
+		}
+	};
+
+	const mouseMoveHandler = (e) => {
+		setValues({...values, error:false, success:false, removed: '',})
+	}
 
 	const newCategoryForm = () => (
 		<form onSubmit={clickSubmit}>
@@ -50,7 +127,17 @@ const Category = () => {
 		</form>
 	);
 
-	return <React.Fragment>{newCategoryForm()}</React.Fragment>;
+	return (
+		<React.Fragment>
+			{showSuccess()}
+			{showError()}
+			{showRemoved()}
+			<div onMouseMove={mouseMoveHandler}>
+				{newCategoryForm()}
+				{showCategories()}
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default Category;
