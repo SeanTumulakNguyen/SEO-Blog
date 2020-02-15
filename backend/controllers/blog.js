0cc96414b51f0d9f3f5ec351e13022eb883ac03b
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-02-15 00:34:03 -0500

    blog controller and routes

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -1,3 +1,29 @@
-exports.time = (req, res) => {
-    res.json({ time: Date().toString()})
-}
+const Blog = require('../models/blog');
+const Category = require('../models/category');
+const Tag = require('../models/tag');
+const formidable = require('formidable');
+const slugify = require('slugify');
+const stripHtml = require('string-strip-html');
+const _ = require('lodash');
+const { errorHandler } = require('../helpers/dbErrorHandler');
+
+exports.create = (req, res) => {
+	let form = new formidable.IncomingForm();
+	form.keepExtensions = true;
+	form.parse(req, (err, fields, files) => {
+		if (err) {
+			return res.status(400).json({
+				error: 'Image could not upload'
+			});
+		}
+		const { title, body, categories, tags } = fields;
+
+		let blog = new Blog();
+		blog.title = title;
+		blog.body = body;
+		blog.slug = slugify(title).toLowerCase();
+		blog.mtitle = `${title} | ${process.env.APP_NAME}`;
+		blog.mdescription = stripHtml(body.substring(0, 160));
+		blog.postedBy = req.user._id;
+	});
+};
