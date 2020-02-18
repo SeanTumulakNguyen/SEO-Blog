e49c2506b5ffa10887b0b47bb8202047ab7e449c
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-02-18 00:39:07 -0500

    blog create form and actions

--- frontend/components/crud/BlogCreate.js
+++ frontend/components/crud/BlogCreate.js
@@ -1,5 +1,92 @@
-const CreateBlog = () => {
-    return <div>
-        <h2>Create Blog Form</h2>
-    </div>
-}
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import Route from 'next/router';
+import dynamic from 'next/dynamic';
+import { withRouter } from 'next/router';
+import { getCookie, isAuth } from '../../actions/auth';
+import { getCategories } from '../../actions/category';
+import { getTags } from '../../actions/tag';
+import { create } from '../../actions/blog';
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+import '../../node_modules/react-quill/dist/quill.snow.css';
+
+const BlogCreate = ({ router }) => {
+	const [ body, setBody ] = useState({});
+	const [ values, setValues ] = useState({
+		error: '',
+		sizeError: '',
+		success: '',
+		formData: '',
+		title: '',
+		hidePublishButton: false
+	});
+
+	const { error, sizeError, success, formData, title, hidePublishButton } = values;
+
+	const publishBlog = (e) => {
+		e.preventDefault();
+		console.log('ready to publish blog');
+	};
+
+	const handleChange = (name) => (e) => {
+		console.log(e.target.value);
+	};
+
+	const handleBody = (e) => {
+		console.log(e);
+	};
+
+	const blogCreateForm = () => {
+		return (
+			<form onSubmit={publishBlog}>
+				<div className="form-group">
+					<label className="text-muted">Title</label>
+					<input type="text" className="form-control" value={title} onChange={handleChange('title')} />
+				</div>
+
+				<div className="form-group">
+					<ReactQuill value={body} placeholder="Write something amazing..." onChange={handleBody} />
+				</div>
+
+				<div>
+					<button type="submit" className="btn btn-primary">
+						Publish
+					</button>
+				</div>
+			</form>
+		);
+	};
+
+	return <div>{blogCreateForm()}</div>;
+};
+
+BlogCreate.modules = {
+    toolbar: [
+        [{ header: '1' }, { header: '2' }, { header: [3, 4, 5, 6] }, { font: [] }],
+        [{ size: [] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        ['link', 'image', 'video'],
+        ['clean'],
+        ['code-block']
+    ]
+};
+ 
+BlogCreate.formats = [
+    'header',
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'link',
+    'image',
+    'video',
+    'code-block'
+];
+
+export default withRouter(BlogCreate);
--- frontend/pages/admin/crud/blog.js
+++ frontend/pages/admin/crud/blog.js
@@ -12,11 +12,8 @@ const Blog = () => {
 						<div className="col-md-12 pt-5 pb-5">
 							<h2>Create a new blog</h2>
 						</div>
-						<div className="col-md-6">
+						<div className="col-md-12">
 							<BlogCreate />
-						</div>
-						<div className="col-md-6">
-							<Tag />
 						</div>
 					</div>
 				</div>
