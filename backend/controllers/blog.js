4a83d1db767d8f6ebe25041141bbbc2d13feeb7d
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-02-24 00:54:42 -0500

    list blogs via postman

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -103,3 +103,36 @@ exports.create = (req, res) => {
 		});
 	});
 };
+
+exports.list = (req, res) => {
+	Blog.find({})
+		.populate('categories', '_id name slug')
+		.populate('tag', '_id name slug')
+		.populate('postedBy', '_id name username')
+		.select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
+		.exec((err, data) => {
+			if (err) {
+				return res.json({
+					error: errorHandler(err)
+				})
+			}
+			res.json({ data })
+		})
+}
+
+exports.listAllBlogsCategoriesTags = (req, res) => {
+
+};
+
+exports.read = (req, res) => {
+
+};
+
+exports.remove = (req, re) => {
+
+}
+
+exports.update = (req, res) => {
+
+}
+
