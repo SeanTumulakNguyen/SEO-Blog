66181912665a3c3f139c1adec103bc81b27e8921
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-03-12 22:03:45 -0400

    creating user routes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,3 +4,8 @@ exports.read = (req, res) => {
 	req.profile.hashed_password = undefined;
 	return res.json(req.profile);
 };
+
+exports.publicProfile = (req, res) => {
+	username = req.params.username
+	User.findOne({})
+}
