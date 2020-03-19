da5345514abc28e54e0bfadc1624b793fb2bdf77
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-03-18 20:15:06 -0400

    routes and profile update

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,8 @@
 const User = require('../models/user');
 const Blog = require('../models/blog');
+const _ = require('lodash');
+const formidable = require('formidable');
+const fs = require('fs');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.read = (req, res) => {
@@ -8,36 +11,88 @@ exports.read = (req, res) => {
 };
 
 exports.publicProfile = (req, res) => {
-    let username = req.params.username;
-    let user;
-    let blogs;
+	let username = req.params.username;
+	let user;
+	let blogs;
 
-    User.findOne({ username }).exec((err, userFromDB) => {
-        if (err || !userFromDB) {
+	User.findOne({ username }).exec((err, userFromDB) => {
+		if (err || !userFromDB) {
+			return res.status(400).json({
+				error: 'User not found'
+			});
+		}
+		user = userFromDB;
+		let userId = user._id;
+		Blog.find({ postedBy: userId })
+			.populate('categories', '_id name slug')
+			.populate('tags', '_id name slug')
+			.populate('postedBy', '_id name')
+			.limit(10)
+			.select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
+			.exec((err, data) => {
+				if (err) {
+					return res.status(400).json({
+						error: errorHandler(err)
+					});
+				}
+				user.photo = undefined;
+				user.hashed_password = undefined;
+				res.json({
+					user,
+					blogs: data
+				});
+			});
+	});
+};
+
+exports.photo = (req, res) => {
+    const username = req.params.username
+
+    User.findOne({ username }).exec((err, user) => {
+        if (err || !user) {
             return res.status(400).json({
                 error: 'User not found'
-            });
+            })
         }
-        user = userFromDB;
-        let userId = user._id;
-        Blog.find({ postedBy: userId })
-            .populate('categories', '_id name slug')
-            .populate('tags', '_id name slug')
-            .populate('postedBy', '_id name')
-            .limit(10)
-            .select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
-            .exec((err, data) => {
-                if (err) {
-                    return res.status(400).json({
-                        error: errorHandler(err)
-                    });
-                }
-                user.photo = undefined;
-                user.hashed_password = undefined;
-                res.json({
-                    user,
-                    blogs: data
-                });
-            });
-    });
+
+        if (user.photo.data) {
+            res.set('Content-Type', user.photo.contentType)
+            return res.send(user.photo.data)
+        }
+    })
+};
+
+exports.update = (req, res) => {
+	let form = new formidable.IncomingForm();
+	form.parse(req, (err, fields, files) => {
+		if (err) {
+			return res.status(400).json({
+				error: 'Photo could not be uploaded'
+			});
+		}
+
+		let user = req.profile;
+
+		user = _.extend(user, fields);
+
+		if (files.photo) {
+			if (files.photo.size > 1000000) {
+				return res.status(400).json({
+					error: 'Image should be less than 1mb'
+				});
+			}
+			user.photo.data = fs.readFileSync(files.photo.path);
+			user.photo.contentType = files.photo.type;
+
+			user.save((err, result) => {
+				if (err) {
+					return res.status(400).json({
+						error: errorHandler(err)
+					});
+				}
+				user.hashed_password = undefined;
+				res.json(user);
+			});
+		}
+	});
 };
