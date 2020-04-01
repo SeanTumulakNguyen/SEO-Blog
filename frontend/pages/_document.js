40dd759ac4785bd48fc28d8ec400e1a631a5142b
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-04-01 00:00:04 -0400

    add google analytics code

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,7 +1,23 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 
 class MyDocument extends Document {
-	render() {
+	setGoogleTags () {
+		if (publicRuntimeConfig.PRODUCTION) {
+			return {
+				__html: `
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+			  
+				gtag('config', 'UA-162413404-1');
+				`
+			};
+		}
+	}
+
+	render () {
 		return (
 			<Html lang="en">
 				<Head>
@@ -12,6 +28,8 @@ class MyDocument extends Document {
 						href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
 					/>
 					<link rel="stylesheet" type="text/css" href="/static/css/styles.css" />
+					<script async src="https://www.googletagmanager.com/gtag/js?id=UA-162413404-1" />
+					<script dangerouslySetInnerHTML={this.setGoogleTags()} />
 				</Head>
 				<body>
 					<Main />
